37eacd2aa040d1c45896e888db710ba38bb0c628
Kanchanparashar <110388011+Kanchanparashar@users.noreply.github.com>
2023-08-04 15:34:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./component/Navbar";
+import About from "./component/About";
+function App() {
+  return (
+    <>
+
+<Navbar/>
+<About/>
+{/* <div className="container my-3">
+<TextForm heading="Enter The text to analyze"/>
+</div> */}
+
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/TextForm.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+export default function TextForm(props) {
+  const handleUpClick = ()=>{
+    console.log("Uppercased"+ text);
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+  const handleOnChange = (event)=>{
+    console.log("On Change");
+    setText(event.target.value)
+  }
+  const [text, setText] = useState('Enter text here');
+  return (
+    <div>
+      <h1>{props.heading}</h1>
+      <div className="mb-3 my-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+        <button className="btn btn-primary my-3" onClick={handleUpClick}>Convert to Uppercase</button>
+      </div>
+    </div>
+  );
+}
+
